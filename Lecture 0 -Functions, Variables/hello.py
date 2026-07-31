5d2858f83486938024b004606b51725c467932a2
zawrer <eldd070218@gmail.com>
2026-07-31 05:09:21 +0300

    [AD] Add variant print

--- Lecture 0 -Functions, Variables/hello.py
+++ Lecture 0 -Functions, Variables/hello.py
@@ -2,4 +2,10 @@
 name = input("What's your name? ")
 
 # Говорим привет пользователю
-print(f"Hello {name}")
+# Разные виды добавления переменных к функции "print"
+#1
+print(f"Hello, {name}")
+#2
+print("Hello,", name)
+#3
+print("Hello, " + name)
